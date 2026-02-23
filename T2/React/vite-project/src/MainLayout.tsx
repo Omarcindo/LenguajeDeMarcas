06f81561abb8560d82957ac439ef595f457a9c62
Omarcindo <omarmonroy0208@gmail.com>
2026-02-23 09:55:35 +0100

    Añadido repo del profesor como submódulo

---
+++ T2/React/vite-project/src/MainLayout.tsx
@@ -0,0 +1,7 @@
+export const MainLayout = () => {
+    return (
+        <div className="min-h-screen w-full bg-gray-100 text-white">
+            <header>
+                
+            </header>
+}
